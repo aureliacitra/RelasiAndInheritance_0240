ad957c42a4eb84f33758d306cf5b41a03aff614f
aureliacitra <citrapangukir@gmail.com>
2025-05-26 20:58:47 +0700

    add string sekolah & add distruktor pelajar

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -27,4 +27,14 @@ class pelajar : public orang
 {
 public :
     string sekolah;
-}
+
+     pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + "dari sekolah " + sekolah + "\n\n"
+    }
+};

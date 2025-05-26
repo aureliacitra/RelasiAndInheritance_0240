2ec529199bfe5f2c3ca9b346fabdcd3c529fd0e8
aureliacitra <citrapangukir@gmail.com>
2025-05-26 21:00:33 +0700

    add pelajar & cout siswa

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -39,4 +39,8 @@ public :
     }
 };
 
-int main() 
+int main() {
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+}
